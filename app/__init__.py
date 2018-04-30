c32b6155e9661f1549f37a1e28ec9f8eac08607e
gamaliny <33547685+gamaliny@users.noreply.github.com>
2018-04-30 18:18:37 +0200

    Ajout de la page d'accueil et de la page d'erreur 404

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-﻿from flask import Flask, abort
+﻿from flask import Flask, abort, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, current_user
 from flask_bootstrap import Bootstrap
@@ -26,14 +26,17 @@ db.create_all()
 
 @application.route('/')
 def home_page():
-    return 'home start'
+    return render_template('home.html')
 
 
 @login.user_loader
 def load_user(user_id):
     return Account.query.get(int(user_id))
 
-
+@application.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+	
 from app.routes.users import users_app
 from app.routes.scan import scan_app
 from app.routes.translation import translation_app
